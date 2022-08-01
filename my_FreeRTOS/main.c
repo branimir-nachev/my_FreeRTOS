18f876f7c694da0ba31b9de75d8203bd677a0422
Branimir Nachev <32915919+branimir-nachev@users.noreply.github.com>
2022-08-01 19:44:03 +0300

    Initial Push

---
+++ my_FreeRTOS/main.c
@@ -0,0 +1,103 @@
+
+
+/**
+ * main.c
+ */
+
+#include <global_include.h>
+#include "utils/uartstdio.h"
+
+//*****************************************************************************
+//
+// Configure the UART and its pins.  This must be called before UARTprintf().
+//
+//*****************************************************************************
+void ConfigureUART(void)
+{
+    //
+    // Enable the GPIO Peripheral used by the UART.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Enable UART0
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    //
+    // Configure GPIO Pins for UART mode.
+    //
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Use the internal 16MHz oscillator as the UART clock source.
+    //
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Initialize the UART for console I/O.
+    //
+    UARTStdioConfig(0, 115200, 16000000);
+}
+
+
+int main(void)
+{
+    volatile uint32_t ui32Loop;
+
+    ConfigureUART();
+
+    UARTprintf("Start program !");
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Check if the peripheral access is enabled.
+    //
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    {
+    }
+
+    //
+    // Enable the GPIO pin for the LED (PG2).  Set the direction as output, and
+    // enable the GPIO pin for digital function.
+    //
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    //
+    // Loop forever.
+    //
+    while(1)
+    {
+        //
+        // Turn on the LED.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
+        //
+        // Delay for a bit.
+        //
+        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        {
+        }
+
+        //
+        // Turn off the LED.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+
+        //
+        // Delay for a bit.
+        //
+        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        {
+        }
+    }
+
+	return 0;
+}
